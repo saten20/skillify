c85158471e9f64ed7ef565831228fc513f126691
satendra <yadavsaten9834@gmail.com>
2024-09-06 15:29:22 +0530

    Review Slider

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -116,7 +116,9 @@ const About = () => {
         <h1 className="text-center text-4xl font-semibold mt-8 text-richblack-800">
           Reviews from other learners
         </h1>
-        <ReviewSlider />
+        <div>
+            <ReviewSlider />
+        </div>
       </div>
       <Footer />
     </div>
